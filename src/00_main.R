062d084291aeb770bdc8494478b79ab8e0b77118
Michal Ziembinski <m.ziembinski@gmail.com>
2019-06-02 15:48:30 +0100

    'Batman'

---
+++ src/00_main.R
@@ -0,0 +1,196 @@
+##________________________________________________##
+####  Earnungs discontinuity - Simulation       ####
+##________________________________________________##
+
+#rm(list=ls())
+
+#### set up the enviroment #####
+#devtools::install_github('cttobin/ggthemr', upgrade_dependencies = F)
+
+packages<-c("data.table","ggplot2",
+            "reshape2",'ggthemr',
+            'tidyverse','gridExtra',
+            'VGAM','MASS','normalp','nortest',
+            'bbmle','evir',
+            'gridExtra')
+
+sapply(packages,require,character.only=T)
+
+options(scipen = 999)
+
+ggthemr('fresh')
+
+## load functons 
+
+source('src/99_functions.R', echo=F)
+
+
+#### define tested variants ####
+
+## earnings
+variants <- list()
+
+variants$earnings <- list()
+
+variants$earnings$name[[1]] <- 'laplace'
+variants$earnings$params[[1]] <- c(0,0.2)
+
+variants$earnings$name[[2]] <- 'norm'
+variants$earnings$params[[2]] <- c(0,0.2)
+
+variants$earnings$name[[3]] <- 'logis'
+variants$earnings$params[[3]] <- c(0,0.2)
+
+variants$earnings$name[[4]] <- 'cauchy'
+variants$earnings$params[[4]] <- c(0,0.2)
+
+## earnings management
+variants$earn_mngt <- list()
+variants$earn_mngt$name[[1]] <- 'norm'
+variants$earn_mngt$params[[1]] <- c(-0.01,0.01)
+
+variants$earn_mngt$name[[2]] <- 'weibull'
+variants$earn_mngt$params[[2]] <- c(-0.01,0.01)
+
+variants$earn_mngt$name[[3]] <- 'gamma'
+variants$earn_mngt$params[[3]] <- c(1,25)
+
+variants$earn_mngt$name[[4]] <- 'exp'
+variants$earn_mngt$params[[4]] <- c(-0.01,0.01)
+
+## management type
+#variants$type$name[[1]] <-'-x'
+variants$type$name[[2]] <- 'norm'
+variants$type$name[[3]] <- '0'
+  
+#### simulate earnings ####
+e = 1
+m = 3
+t = 2
+
+print(variants$earnings$name[[e]])
+print(variants$earn_mngt$name[[m]])
+print(variants$type$name[[t]])
+
+for(i in 1:100){
+  temp <- simulate_earnings(earnings = variants$earnings$name[[e]],
+                            params = variants$earnings$params[[e]],
+                            earn_mngt = variants$earn_mngt$name[[m]],
+                            earn_mngt_params = variants$earn_mngt$params[[m]],
+                            occur = 0.01,
+                            type = variants$type$name[[t]])
+  
+  temp[,i := i]
+  
+  dataplotBD <- temp[,BD_test(x_mngd,
+                              output='all',
+                              range_h=0.005,
+                              lim1=-0.2)][,.(range, N, N_f, N_diff, N_lag, N_lead, sd_diff, t_stat, type = 'managed')]
+  dataplotBD <- rbind(dataplotBD,
+                      temp[,BD_test(x,
+                                    output='all',
+                                    range_h=0.005,
+                                    lim1=-0.2)][,.(range, N, N_f, N_diff, N_lag, N_lead, sd_diff, t_stat, type = 'un_mananged')])
+  
+  dataplotBD[,i := i]
+  
+  ifelse(i == 1,
+         data_simulation <- temp,
+         data_simulation <- rbind(data_simulation,
+                                  temp))
+  ifelse(i == 1,
+         data_test <- dataplotBD,
+         data_test <- rbind(data_test,
+                            dataplotBD))
+  if(i%%100 == 0){
+    print(Sys.time())
+    print(i)
+  }
+}
+
+#data_test[abs(range) < 0.01 & i == 5]
+
+ggplot(data_test[range == 0],
+       aes(abs(t_stat))) + 
+  geom_histogram(bins = 50) + 
+  geom_vline(xintercept = qnorm(0.99), col = 'red', lty = 3, size = 2) +
+  facet_grid(type ~ .) +
+  theme_light()
+
+#### test plots ####
+
+## N
+p1 <- ggplot(dataplotBD,
+       aes(range, N, col = type)) +
+  geom_step() +
+  geom_vline(aes(xintercept = 0), col = 'red', lty = 3) +
+  theme(legend.position = 'bottom')
+
+dataplot <- data_test[,.(Q25 = quantile(N,0.25),
+                         Q50 = quantile(N,0.50),
+                         Q75 = quantile(N,0.75)), by = .(range,type)]
+
+p2 <- ggplot(dataplot,
+       aes(range, Q50, col = type, fill = type)) +
+  geom_ribbon(aes(ymin = Q25, ymax = Q75), alpha = 0.4) +
+  geom_step() +
+  geom_vline(aes(xintercept = 0), col = 'red', lty = 3) +
+  theme(legend.position = 'bottom')
+
+## t_stat
+p3 <- ggplot(dataplotBD,
+       aes(range, t_stat, col = type)) +
+  geom_step() +
+  geom_vline(aes(xintercept = 0), col = 'red', lty = 3) +
+  theme(legend.position = 'bottom')
+
+dataplot <- data_test[,.(Q25 = quantile(t_stat,0.25),
+                         Q50 = quantile(t_stat,0.50),
+                         Q75 = quantile(t_stat,0.75)), by = .(range,type)]
+
+p4 <- ggplot(dataplot,
+       aes(range, Q50, col = type, fill = type)) +
+  geom_ribbon(aes(ymin = Q25, ymax = Q75), alpha = 0.4) +
+  geom_step() +
+  geom_vline(aes(xintercept = 0), col = 'red', lty = 3) +
+  theme(legend.position = 'bottom')
+
+p <- grid.arrange(p1,p2,p3,p4)
+
+##_____________##
+#### working ####
+##_____________##
+temp <- simulate_earnings(earnings = 'norm', earn_mngt = 'norm')
+temp <- simulate_earnings(earnings = 'laplace', earn_mngt = 'norm')
+
+temp <- simulate_earnings(earnings = 'laplace',
+                          earn_mngt = 'gamma',
+                          earn_mngt_params = c(1,25),
+                          type = 'norm')
+
+#temp <- simulate_earnings(earnings = 'logis', earn_mngt = 'norm')
+#temp <- simulate_earnings(earnings = 'cauchy', earn_mngt = 'norm')
+temp[x > -0.01 & x < 0]
+temp[flag_mngd == 1]
+
+ggplot(temp,aes(x)) + 
+  geom_histogram(bins = 50) + 
+  geom_vline(xintercept = 0, col = 'red', lty = 3, size = 2) +
+  theme_light()
+
+ggplot(temp,aes(x,em_prob)) +
+  geom_vline(xintercept = 0, col = 'red', lty = 3, size = 1) +
+  geom_point() +
+  xlim(-0.4,0.4) +
+  theme_light()
+
+#managed
+ggplot(temp,aes(x_mngd)) + 
+  geom_histogram(bins = 50) + 
+  geom_vline(xintercept = 0, col = 'red', lty = 3, size = 2) +
+  theme_light()
+
+
+#### run test ####
+
+
---
+++ src/99_functions.R
@@ -0,0 +1,101 @@
+#' Add together two numbers.
+#' 
+#' @param size A number of companies to be simulated.
+#' @param earnings A name of distribution to simulate un-managed earnings. One of c('laplace','norm','logis','cauchy')
+#' @param params The parameters of the distribution function. 
+#' @param earn_mngt The name of the distribution to simulate earnings management. One of c('norm','weibull','gamma','exp') 
+#' @param earn_mngt_params The parameters of the distribution function to simulate managed earnings.
+#' @param occur What percentage of companies decides to manage.
+#' @param type The type of earnings management. One of c('-x','norm','0')
+#' @return The dataset with all observations.
+#' @examples
+#' simulate_earnings()
+#' simulate_earnings(earnings = 'norm', earn_mngt = 'norm')
+
+
+simulate_earnings <- function(size = 10000,
+                              earnings = 'laplace',
+                              params = c(0,0.2),
+                              earn_mngt = 'norm',
+                              earn_mngt_params = c(-0.01,0.01),
+                              occurr = 0.1,
+                              type = 'none'){
+  
+  ## simulate earnings
+  if(earnings == 'laplace') sim_earn <- round(rlaplace(size, 
+                                                       location = params[1],
+                                                       scale = params[2]),4)
+  if(earnings == 'norm') sim_earn <- round(rnorm(size, 
+                                                 mean = params[1],
+                                                 sd = params[2]),4)
+  if(earnings == 'logis') sim_earn <- round(rlogis(size, 
+                                                   location = params[1],
+                                                   scale = params[2]),4)
+  if(earnings == 'cauchy') sim_earn <- round(rcauchy(size, 
+                                                     location = params[1],
+                                                     scale = params[2]),4)
+  
+  sim_data <- data.table(x = sim_earn)  
+
+  
+  ##  simulate earmings management 
+  
+  #### earn mgnt probability
+  if(earn_mngt == 'norm'){
+    sim_data[,em_prob := round(dnorm(x,
+                              mean = earn_mngt_params[1],
+                              sd = earn_mngt_params[2]),4)]
+  }
+  if(earn_mngt == 'weibull'){
+    sim_data[,em_prob := round(dweibull(x,
+                                     earn_mngt_params[1],
+                                     earn_mngt_params[2]),4)]
+  }
+  if(earn_mngt == 'gamma'){
+    sim_data[,em_prob := round(dgamma(-x,
+                                      shape = earn_mngt_params[1],
+                                      rate = earn_mngt_params[2]),4)]
+  }
+  if(earn_mngt == 'exp'){
+    sim_data[,em_prob := round(dexp(x,
+                                     rate = earn_mngt_params[1]),4)]
+  }
+  
+  ##  managed earnings 
+  sim_data[,id := .I]
+  #sim_data[,flag_mngd := rbinom(1, size=2, prob=em_prob/100), by = id]
+  sim_data[,flag_mngd := runif(1) * occurr, by = id]
+  sim_data[,flag_mngd := ifelse(em_prob > flag_mngd, 1, 0)]
+  sim_data[,x_mngd := x]
+  
+  if(type == '-x')  sim_data[flag_mngd == 1, x_mngd := -x]
+  if(type == 'norm')  sim_data[flag_mngd == 1, x_mngd := abs(rnorm(1,0.01,0.01)), by = id]
+  if(type == '0')  sim_data[flag_mngd == 1, x_mngd := 0]
+  
+  return(sim_data)
+  
+}
+
+
+
+##_________________________________________________________##
+#### BD test ####
+##_________________________________________________________##
+
+BD_test<-function(x,range_h=0.0025,lim1=-0.1,lim2=-lim1,output='t_stat'){
+  x<-data.table('x'=x)
+  x[,range:=floor(x/range_h)*range_h]
+  x<-x[,.N,by=range][order(range)]
+  x[,N_f:=as.numeric(stats::filter(N,c(1/2,0,1/2)))]
+  x[,N_diff:=N-N_f]
+  x[,N_lag:=shift(N,type='lag')]
+  x[,N_lead:=shift(N,type='lead')]
+  N_all<-x[,sum(N)]
+  x[,sd_diff:=N_all*(N/N_all)*(1-N/N_all)+
+      (1/4)*N_all*(N_lag/N_all+N_lead/N_all)*(1-N_lag/N_all-N_lead/N_all)]
+  x[,t_stat:=N_diff/sqrt(sd_diff)]
+  if(output=='t_stat'){return(x[range>=lim1&range<=lim2,c('range','t_stat'),with=F])}
+  if(output=='N'){return(x[range>=lim1&range<=lim2,c('range','N'),with=F])}
+  if(output=='all'){return(x[range>=lim1&range<=lim2])}
+}
+
