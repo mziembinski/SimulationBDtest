ff58e37c6f8dce0cd89ba5cc61f40dec7fc2e833
mziembinski <m.ziembinski@gmail.com>
2019-09-08 11:16:37 +0100

    modify simulation

--- src/00_main.R
+++ src/00_main.R
@@ -22,6 +22,7 @@ ggthemr('fresh')
 
 ## load functons 
 
+setwd('C:/Users/Paulina/Desktop/Misiowe/SimulationBDtest')
 source('src/99_functions.R', echo=F)
 
 
@@ -32,40 +33,209 @@ variants <- list()
 
 variants$earnings <- list()
 
-variants$earnings$name[[1]] <- 'laplace'
-variants$earnings$params[[1]] <- c(0,0.2)
+variants$earnings$name[[1]] <- 'norm'
+variants$earnings$params[[1]] <- c(0.035,0.05)
 
-variants$earnings$name[[2]] <- 'norm'
-variants$earnings$params[[2]] <- c(0,0.2)
+variants$earnings$name[[2]] <- 'logis'
+variants$earnings$params[[2]] <- c(0.036,0.03)
 
-variants$earnings$name[[3]] <- 'logis'
-variants$earnings$params[[3]] <- c(0,0.2)
+variants$earnings$name[[3]] <- 'laplace'
+variants$earnings$params[[3]] <- c(0.03,0.05)
 
 variants$earnings$name[[4]] <- 'cauchy'
-variants$earnings$params[[4]] <- c(0,0.2)
+variants$earnings$params[[4]] <- c(0.03,0.04)
+
+variants$earnings$name[[5]] <- 'laplace'
+variants$earnings$params[[5]] <- c(-0.03,0.05)
+
+variants$earnings$name[[6]] <- 'laplace'
+variants$earnings$params[[6]] <- c(0,0.05)
+
+variants$earnings$name[[7]] <- 'laplace'
+variants$earnings$params[[7]] <- c(0.01,0.05)
+
+variants$earnings$name[[8]] <- 'laplace'
+variants$earnings$params[[8]] <- c(-0.01,0.05)
+
+variants$earnings$name[[9]] <- 'laplace'
+variants$earnings$params[[9]] <- c(0.005,0.05)
+
+variants$earnings$name[[10]] <- 'laplace'
+variants$earnings$params[[10]] <- c(-0.005,0.05)
 
 ## earnings management
 variants$earn_mngt <- list()
-variants$earn_mngt$name[[1]] <- 'norm'
-variants$earn_mngt$params[[1]] <- c(-0.01,0.01)
+
+variants$earn_mngt$name[[1]] <- 'exp'
+variants$earn_mngt$params[[1]] <- c(20,NA,0.05)
 
 variants$earn_mngt$name[[2]] <- 'weibull'
-variants$earn_mngt$params[[2]] <- c(-0.01,0.01)
+variants$earn_mngt$params[[2]] <- c(1.1,0.05,0.042)
 
 variants$earn_mngt$name[[3]] <- 'gamma'
-variants$earn_mngt$params[[3]] <- c(1,25)
+variants$earn_mngt$params[[3]] <- c(1.2,25,0.044)
 
-variants$earn_mngt$name[[4]] <- 'exp'
-variants$earn_mngt$params[[4]] <- c(-0.01,0.01)
 
 ## management type
-#variants$type$name[[1]] <-'-x'
-variants$type$name[[2]] <- 'norm'
-variants$type$name[[3]] <- '0'
-  
+variants$type$name[[1]] <-'0'
+variants$type$name[[2]] <- 'norm_sd'
+variants$type$name[[3]] <- 'norm_0.5sd'
+
+
+#### plots ###
+
+#range_h
+#size
+
+ee <- 1
+m <- 1
+t <- 1
+
+
+for (ee in 1:4){
+  for (m in 1:3){
+    for (t in 1:3){
+      range_h <- 0.01
+      
+      temp <- simulate_earnings(earnings = variants$earnings$name[[ee]],
+                                params = variants$earnings$params[[ee]],
+                                earn_mngt = variants$earn_mngt$name[[m]],
+                                earn_mngt_params = variants$earn_mngt$params[[m]],
+                                occur = 0.4,
+                                type = variants$type$name[[t]],
+                                range_h=range_h)
+      
+      #ggplot(temp,
+      #      aes(range + range_h/2,N_final))+
+      # geom_vline(xintercept = 0, col = 'red') +
+      # geom_bar(stat='identity')+
+      #  theme_bw() 
+      
+      temp1<-copy(temp)
+      temp1[,N_final:=NULL]
+      temp1[,N:=sum(N,(-1)*N_mngt,na.rm=T),by=id]
+      temp1[N<0,N:=0]
+      temp1[,id:=NULL]
+      temp1<-data.table(melt(temp1,id.vars = c('range')))
+      
+      temp1$variable <- factor(temp1$variable,
+                               levels = c('N_mngt','N_mngt2','N'))
+      
+      ggplot(temp1,
+             aes(range + range_h/2,value ,fill=variable))+
+        geom_vline(xintercept = 0, col = 'red') +
+        geom_bar(stat='identity', position = 'stack')+
+        scale_fill_manual( values=c("grey30", "grey50", "grey70"))+
+        theme_bw()+theme(legend.position = "none",legend.title = element_blank(),axis.title.y = element_blank(),axis.title.x = element_blank())+
+        ggtitle(paste0('Symulacja (h=1%, N=100000, k=40%): ',variants$earnings$name[[ee]],', ',variants$earn_mngt$name[[m]],', ',variants$type$name[[t]]))+
+        coord_cartesian(xlim=c(-0.2,0.2))+
+        ggsave(paste0('../PHDthesis/simulation_input/sym_example_',ee,m,t,'.pdf'),width=7,height=5)
+      
+
+    }
+  }
+}
+
+#### test size ####
+
+ranges <- c(0.0025, 0.005, 0.01)
+sizes <- c(1e3, 1e4, 1e5)
+
+
+rm(resultsSize)
+
+for (ee in 1:10){
+  for (s in 1:3){
+    for (r in 1:3){
+      
+      for(j in 1:1e5){
+        temp <- simulate_earnings(size = sizes[s],
+                                  earnings = variants$earnings$name[[ee]],
+                                  params = variants$earnings$params[[ee]],
+                                  range_h=ranges[r],
+                                  print = F,
+                                  simulateMngt = F)
+        
+        temp[, N := N]
+        
+        results <- BD_test(temp, range = ranges[r], output = 'all')
+        results[,size := sizes[s]]
+        results[,earnings := variants$earnings$name[[ee]]]
+        results[,params := variants$earnings$params[[ee]]]
+        results[,range_h := ranges[r]]
+        results[,j := j]
+        
+        ifelse(!exists('resultsSize'),
+               resultsSize <- results,
+               resultsSize <- rbind(resultsSize,
+                                    results))
+      }
+    }
+  }
+  print(ee)
+  print(Sys.time())
+}
+
+save(resultsSize, file = 'data/resultsSize.RData')
+
+#### test power ####
+rm(resultsPower)
+
+occurs <- c(0.2, 0.4, 0.6)
+
+for (ee in 1:10){
+  for (m in 1:3){
+    for (t in 1:3){
+      for (o in 1:3){
+        for (s in 1:3){
+          for (r in 1:3){
+            
+            for(j in 1:1e5){
+              temp <- simulate_earnings(size = sizes[s],
+                                        earnings = variants$earnings$name[[ee]],
+                                        params = variants$earnings$params[[ee]],
+                                        earn_mngt = variants$earn_mngt$name[[m]],
+                                        earn_mngt_params = variants$earn_mngt$params[[m]],
+                                        occur = occurs[o],
+                                        type = variants$type$name[[t]],
+                                        range_h=ranges[r],
+                                        print = F,
+                                        simulateMngt = T)
+              
+              temp[, N := N_final]
+              
+              results <- BD_test(temp, range = ranges[r], output = 'all')
+              results[,size := sizes[s]]
+              results[,earnings := variants$earnings$name[[ee]]]
+              results[,params := variants$earnings$params[[ee]]]
+              results[,earn_mngt := variants$earn_mngt$name[[m]]]
+              results[,earn_mngt_params := variants$earn_mngt$params[[m]]]
+              results[,occur := occurs[o]]
+              results[,type := variants$type$name[[t]]]
+              
+              results[,range_h := ranges[r]]
+              results[,j := j]
+              
+              ifelse(!exists('resultsPower'),
+                     resultsPower <- results,
+                     resultsPower <- rbind(resultsPower,
+                                           results))
+            }
+          }
+        }
+      }
+    }
+  }
+  print(ee)
+  print(Sys.time())
+}
+
+
+save(resultsPower, file = 'data/resultsPower.RData')
+
 #### simulate earnings ####
 e = 1
-m = 3
+m = 1
 t = 2
 
 print(variants$earnings$name[[e]])
@@ -77,7 +247,7 @@ for(i in 1:100){
                             params = variants$earnings$params[[e]],
                             earn_mngt = variants$earn_mngt$name[[m]],
                             earn_mngt_params = variants$earn_mngt$params[[m]],
-                            occur = 0.5,
+                            occur = 0.4,
                             type = variants$type$name[[t]])
   
   temp[,i := i]
--- src/99_functions.R
+++ src/99_functions.R
@@ -3,10 +3,11 @@
 #' @param size A number of companies to be simulated.
 #' @param earnings A name of distribution to simulate un-managed earnings. One of c('laplace','norm','logis','cauchy')
 #' @param params The parameters of the distribution function. 
-#' @param earn_mngt The name of the distribution to simulate earnings management. One of c('norm','weibull','gamma','exp') 
+#' @param earn_mngt The name of the distribution to simulate earnings management. One of c('weibull','gamma','exp') 
 #' @param earn_mngt_params The parameters of the distribution function to simulate managed earnings.
 #' @param occur What percentage of companies decides to manage.
-#' @param type The type of earnings management. One of c('norm','0')
+#' @param type The type of earnings management. One of c('norm_sd','norm_0.5sd,'0')
+#' @param range_h The width of the histogram to align simulated distributions.
 #' @return The dataset with all observations.
 #' @examples
 #' simulate_earnings()
@@ -15,11 +16,14 @@
 
 simulate_earnings <- function(size = 10000,
                               earnings = 'laplace',
-                              params = c(0,0.2),
-                              earn_mngt = 'norm',
-                              earn_mngt_params = c(-0.01,0.01),
-                              occurr = 0.1,
-                              type = 'none'){
+                              params = c(0.03,0.04),
+                              earn_mngt = 'exp',
+                              earn_mngt_params = c(20,NA,0.05),#last is the sd dev 
+                              occurr = 0.4,
+                              type = 'norm_sd',
+                              range_h=0.0025,
+                              print = T,
+                              simulateMngt = T){
   
   ## simulate earnings
   if(earnings == 'laplace') sim_earn <- round(rlaplace(size, 
@@ -36,46 +40,110 @@ simulate_earnings <- function(size = 10000,
                                                      scale = params[2]),4)
   
   sim_data <- data.table(x = sim_earn)  
+  
+  
+  sim_data[,range:=floor(x/range_h)*range_h]
+  sim_data<-sim_data[,.N,by=range][order(range)]
+  
+  N0<-round(sum(sim_data[range<0&range>(-0.2)]$N)*occurr)
 
   
   ##  simulate earmings management 
   
-  #### earn mgnt probability
-  if(earn_mngt == 'norm'){
-    sim_data[,em_prob := round(dnorm(x,
-                              mean = earn_mngt_params[1],
-                              sd = earn_mngt_params[2]),4)]
+  if(simulateMngt){
+    #### earn mgnt probability
+    # if(earn_mngt == 'norm'){
+    #    sim_data[,em_prob := round(dlnorm(x,
+    #                              mean = earn_mngt_params[1],
+    #                              sd = earn_mngt_params[2]),4)]
+    #  }
+    if(earn_mngt == 'weibull'){
+      mngt= round(rweibull(N0,earn_mngt_params[1], earn_mngt_params[2]),4)*(-1)
+    }
+    if(earn_mngt == 'gamma'){
+      mngt =round(rgamma(N0,shape = earn_mngt_params[1],rate = earn_mngt_params[2]),4)*(-1)
+    }
+    if(earn_mngt == 'exp'){
+      mngt = round(rexp(N0,rate = earn_mngt_params[1]),4)*(-1)
+    }
+    
+    mngt <- data.table(x = mngt)  
+    mngt[,range:=floor(x/range_h)*range_h]
+    mngt<-mngt[,.N,by=range][order(range)]
+    setnames(mngt,'N','N_mngt')
+    mngt<-mngt[range<0]
+    
+    
+    sim_data<-merge(sim_data,mngt,by='range',all=T)
+    
+    #if simulation of managed gives more observations in a given interval than simulation fo whole sample - move them to the interval closer to zero
+    setkey(sim_data,range)
+    sim_data[,id := .I]
+    sim_data[,N:=as.numeric(N)]
+    sim_data[,N_mngt:=as.numeric(N_mngt)]
+    
+    
+    #sim_data[,range_lag:=shift(range,type='lead')]
+    #wrong<-sim_data[(is.na(N)&!is.na(N_mngt))|(N_mngt>N)]
+    #while(nrow(wrong)>0){
+    #  wrong[,diff:=sum((-1)*N,N_mngt,na.rm=T),by=id]
+    #  
+    #  #print(nrow(wrong))
+    #  
+    #  for (i in 1:nrow(wrong)){
+    #    #print(i)
+    #    sim_data[range==wrong$range[i],N_mngt:=sum(N_mngt,(-1)*wrong$diff[i],na.rm=T),by=id]
+    #    sim_data[range==wrong$range_lag[i],N_mngt:=sum(N_mngt,wrong$diff[i],na.rm=T),by=id]
+    #  }
+    #  
+    #  sim_data[N_mngt==0,N_mngt:=NA]
+    #  wrong<-sim_data[(is.na(N)&!is.na(N_mngt))|(N_mngt>N)]
+    #}
+    
+    wrong<-sim_data[is.na(N)&!is.na(N_mngt)]
+    
+    if(print){
+      print(paste0('usuwamy ',round(sum(wrong$N_mngt,na.rm=T)/N0*100,2),' %'))
+    }
+    
+    
+    sim_data<-sim_data[!is.na(N)]
+    #sim_data[,range_lag:=NULL]
+    
+    sum(sim_data$N,na.rm=T)
+    sum(sim_data$N_mngt,na.rm=T)
+    
+    sim_data[,N_final:=sum(N,(-1)*N_mngt,na.rm=T),by=id]
+    sim_data[N_final<0,N_final:=0]
+    
+    
+    if(type == '0'){
+      sim_data[range==0,N_mngt2:=N0]
+      sim_data[range==0,N_final:=N_final+N0]
+    }
+    if(type == 'norm_sd'){
+      sim_mngt<-abs(rnorm(N0,0,earn_mngt_params[3]))
+      sim_mngt <- data.table(x = sim_mngt)  
+      sim_mngt[,range:=floor(x/range_h)*range_h]
+      sim_mngt<-sim_mngt[,.N,by=range][order(range)]
+      setnames(sim_mngt,'N','N_mngt2')
+      sim_data<-merge(sim_data,sim_mngt,by='range',all=T)
+      sim_data[,N_final:=sum(N_final,N_mngt2,na.rm=T),by=id]
+      
+    }  
+    if(type == 'norm_0.5sd'){
+      sim_mngt<-abs(rnorm(N0,0,earn_mngt_params[3]*0.5))
+      sim_mngt <- data.table(x = sim_mngt)  
+      sim_mngt[,range:=floor(x/range_h)*range_h]
+      sim_mngt<-sim_mngt[,.N,by=range][order(range)]
+      setnames(sim_mngt,'N','N_mngt2')
+      sim_data<-merge(sim_data,sim_mngt,by='range',all=T)
+      sim_data[,N_final:=sum(N_final,N_mngt2,na.rm=T),by=id]
+      
+    }  
   }
-  if(earn_mngt == 'weibull'){
-    sim_data[,em_prob := round(dweibull(x,
-                                     earn_mngt_params[1],
-                                     earn_mngt_params[2]),4)]
-  }
-  if(earn_mngt == 'gamma'){
-    sim_data[,em_prob := round(dgamma(-x,
-                                      shape = earn_mngt_params[1],
-                                      rate = earn_mngt_params[2]),4)]
-  }
-  if(earn_mngt == 'exp'){
-    sim_data[,em_prob := round(dexp(x,
-                                     rate = earn_mngt_params[1]),4)]
-  }
-  
-  ##  managed earnings 
-  sim_data[,id := .I]
-  #sim_data[,flag_mngd := rbinom(1, size=2, prob=em_prob/100), by = id]
-  sim_data[,flag_mngd := runif(1), by = id]
-  sim_data[,flag_mngd := ifelse(em_prob > flag_mngd, 1, 0)]
-  
-  sim_data[,flag_mngdocr := runif(1), by = id]
-  sim_data[,flag_mngd := ifelse(occurr > flag_mngd, flag_mngd, 0)]
-  
-  sim_data[,flag_mngdocr := NULL]
-  sim_data[,x_mngd := x]
-  
-  if(type == 'norm')  sim_data[flag_mngd == 1, x_mngd := abs(rnorm(1,0.01,0.01)), by = id]
-  if(type == '0')  sim_data[flag_mngd == 1, x_mngd := 0]
-  
+  #sim_data[,id:=NULL]
+  #sim_data<-sim_data[,.(range,N_final)]
   return(sim_data)
   
 }
@@ -87,9 +155,9 @@ simulate_earnings <- function(size = 10000,
 ##_________________________________________________________##
 
 BD_test<-function(x,range_h=0.0025,lim1=-0.1,lim2=-lim1,output='t_stat'){
-  x<-data.table('x'=x)
-  x[,range:=floor(x/range_h)*range_h]
-  x<-x[,.N,by=range][order(range)]
+  #x<-data.table('x'=x)
+  #x[,range:=floor(x/range_h)*range_h]
+  #x<-x[,.N,by=range][order(range)]
   x[,N_f:=as.numeric(stats::filter(N,c(1/2,0,1/2)))]
   x[,N_diff:=N-N_f]
   x[,N_lag:=shift(N,type='lag')]
